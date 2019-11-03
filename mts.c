ddbaf601129db789b00ac70d61d2d815715fc074
Nicholas Roethel <roetheln@gmail.com>
2019-11-03 12:46:03 -0800

    newest

--- mts.c
+++ mts.c
@@ -179,7 +179,7 @@ int main(int argc, char *argv[]){
 
   for(t=0;t<NUM_THREADS;t++){
     printf("In main: creating thread %ld\n", t);
-       rc = pthread_create(&threads[t], NULL, waitForTime(5), (void *)t);
+      rc = pthread_create(&threads[t], NULL, waitForTime(), 5);
       if (rc){
         printf("ERROR; return code from pthread_create() is %d\n", rc);
         exit(-1);
