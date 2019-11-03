5e86e02f1b65f9a0aaecaa3a027af9bb1c691736
Nicholas Roethel <roetheln@gmail.com>
2019-11-03 13:38:47 -0800

    newest

--- mts.c
+++ mts.c
@@ -161,7 +161,7 @@ int main(int argc, char *argv[]){
 
   loadingCurrent = loadingHead;
 
-  t = 0;
+  t = 1;
   while(loadingCurrent->next !=NULL){
     printf("In main: creating thread %ld\n", t);
     rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
