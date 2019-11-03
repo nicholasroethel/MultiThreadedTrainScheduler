b63b7dbf41aee6e03656955b60153df3da7c1e83
Nicholas Roethel <roetheln@gmail.com>
2019-11-03 00:16:46 -0700

    newest

--- mts.c
+++ mts.c
@@ -41,14 +41,14 @@ void loadTrain(struct Train train){
 
 void printLoading (struct loading *loadingHead, struct loading *loadingCurrent){
   loadingCurrent = loadingHead;
-  printf("Train\n");
-  printf("%c",(loadingCurrent->train.direction));
-  printf("%d",loadingCurrent->train.loadTime);
+
+  printf("%c ",(loadingCurrent->train.direction));
+  printf("%d ",loadingCurrent->train.loadTime);
   printf("%d\n",loadingCurrent->train.crossTime);
   while(loadingCurrent->next != NULL){
     loadingCurrent = loadingCurrent->next;
-    printf("%c",loadingCurrent->train.direction);
-    printf("%d",loadingCurrent->train.loadTime);
+    printf("%c ",loadingCurrent->train.direction);
+    printf("%d ",loadingCurrent->train.loadTime);
     printf("%d\n",loadingCurrent->train.crossTime);
 
   }
