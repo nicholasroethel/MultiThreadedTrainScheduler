22690da62e4bd988c9ee48fb348178223c6be9b2
Nicholas Roethel <roetheln@gmail.com>
2019-11-03 00:36:58 -0700

    newest

--- mts.c
+++ mts.c
@@ -114,16 +114,19 @@ int main(int argc, char *argv[]){
     //get the trains direction
     token = strtok(line, delim);
     tempTrain->direction = *token;
+
     printf( "%s ", token );
 
     //get the trains load time
     token = strtok(NULL, delim);
     tempTrain->loadTime = *token;
+    printf("%s ",*token );
     printf( "%s ", token );
 
     //get the trains crossing time
     token = strtok(NULL, delim);
     tempTrain->crossTime = *token;
+    printf("%s ",*token );
     printf( "%s \n", token ); 
 
     //increment train counter
